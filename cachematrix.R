39735bdecd62a617bd2055cab7f041f9ff3bc1f1
gabriel-laguardia <33882529+gabriel-laguardia@users.noreply.github.com>
2017-12-29 23:21:55 -0200

    Implemented the methods
    Commit for the Programming Assignment 2: Lexical Scoping

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,52 @@
-## Put comments here that give an overall description of what your
-## functions do
+##  This is a pair of functions that calculates and cache the inverse of a matrix.
 
-## Write a short comment describing this function
+## This function gives the methods for storing a matrix and its cached inverse
 
 makeCacheMatrix <- function(x = matrix()) {
+	
+        ## Creates inv cleared
+        inv <- NULL
 
+        set <- function(newmatrix) {
+
+                ## Sets new matrix
+                x <<- newmatrix
+
+                ## Clears inv if matrix changes
+                inv <<- NULL
+        }
+
+        get <- function() x
+
+        ## Cache the inv (but does not make the calculation)
+        setinv <- function(inverse) inv <<- inverse
+
+        ## Returns the inv cached (but does not make the calculation)
+        getinv <- function() inv
+
+        list(set = set, get = get,
+                 setinv = setinv,
+                 getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## This function solves the inv of the given CacheMatrix if the value is not already cached
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+	
+        ## Returns the inv cached (or NULL)
+        inv <- x$getinv()
+
+        ## If cache is set
+        if(!is.null(inv)) {
+
+                message("getting cached data")
+                return(inv)
+        }
+
+        ## Make the calculation
+        matr <- x$get()
+        inv <- solve(matr, ...)
+        x$setinv(inv)
+        inv
 }
